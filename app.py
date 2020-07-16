d420856679c391dde73542ddd73f0b9ee8a07bb6
Saad Bensalim <sabensalim@brooklynfriends.org>
2020-07-16 13:20:31 -0400

    First commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import model
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/quiz')
+def quiz():
+    return render_template('quiz.html')
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    headset = request.form["platform"]
+    genre = request.form["genre"]
+    setup = request.form["setup"]
+    headset_name = model.headsetchoice(headset, genre, setup)
+    print(headset_name)
+    return render_template('results.html', setup = setup, genre = genre, answer=headset, headset_name=headset_name)
---
+++ model.py
@@ -0,0 +1,212 @@
+def headsetchoice(answer, genre, setup):
+    if answer == "pc" and genre == "battleroyale" and "desk":
+        return "Cloud Alpha"
+    elif answer == "pc" and genre == "battleroyale" and "livingroom":
+        return "Logitech G Pro"
+    elif answer == "pc" and genre == "battleroyale" and "office":
+        return "Turtle Beach 50P"
+    elif answer == "pc" and genre == "rpg" and "desk":
+        return "Arctis 1"
+    elif answer == "pc" and genre == "rpg" and "livingroom":
+        return "Razer Kraken"
+    elif answer == "pc" and genre == "rpg" and "office":
+        return "Arctis 1"
+    elif answer == "pc" and genre == "fps" and "desk":
+        return "Logitech G Pro"
+    elif answer == "pc" and genre == "fps" and "livingroom":
+        return "Cloud Alpha"
+    elif answer == "pc" and genre == "fps" and "office":
+        return "Razer Kraken"
+    elif answer == "pc" and genre == "sim" and "desk":
+        return "Turtle Beach 50P"
+    elif answer == "pc" and genre == "sim" and "livingroom":
+        return "Arctis 1"
+    elif answer == "pc" and genre == "sim" and "office":
+        return "Logitech G Pro"
+    elif answer == "pc" and genre == "strategy" and "desk":
+        return "Cloud Alpha"
+    elif answer == "pc" and genre == "strategy" and "livingroom":
+        return "Razer Kraken"
+    elif answer == "pc" and genre == "strategy" and "desk":
+        return "Turtle Beach 50P"
+    elif answer == "phone" and genre == "battleroyale" and "desk":
+        return "Cloud Alpha"
+    elif answer == "phone" and genre == "battleroyale" and "livingroom":
+        return "Logitech G Pro"
+    elif answer == "phone" and genre == "battleroyale" and "office":
+        return "Turtle Beach 50P"
+    elif answer == "phone" and genre == "rpg" and "desk":
+        return "Arctis 1"
+    elif answer == "phone" and genre == "rpg" and "livingroom":
+        return "Razer Kraken"
+    elif answer == "phone" and genre == "rpg" and "office":
+        return "Arctis 1"
+    elif answer == "phone" and genre == "fps" and "desk":
+        return "Logitech G Pro"
+    elif answer == "phone" and genre == "fps" and "livingroom":
+        return "Cloud Alpha"
+    elif answer == "phone" and genre == "fps" and "office":
+        return "Razer Kraken"
+    elif answer == "phone" and genre == "sim" and "desk":
+        return "Turtle Beach 50P"
+    elif answer == "phone" and genre == "sim" and "livingroom":
+        return "Arctis 1"
+    elif answer == "phone" and genre == "sim" and "office":
+        return "Logitech G Pro"
+    elif answer == "phone" and genre == "strategy" and "desk":
+        return "Cloud Alpha"
+    elif answer == "phone" and genre == "strategy" and "livingroom":
+        return "Razer Kraken"
+    elif answer == "phone" and genre == "strategy" and "desk":
+        return "Turtle Beach 50P"
+    elif answer == "nintendo switch" and genre == "battleroyale" and setup == "desk":
+        return "Cloud Alpha"
+    elif answer == "nintendo switch" and genre == "rpg" and setup == "desk":
+        return "Razer Kraken" 
+    elif answer == "nintendo switch" and genre == "fps" and setup == "desk":
+        return "Arctis 1"
+    elif answer == "nintendo switch" and genre == "sim" and setup == "desk":
+        return "Turtle Beach 50P"
+    elif answer == "nintendo switch" and genre == "sim" and setup == "desk":
+        return "Logitech G Pro"
+    
+    elif answer == "nintendo switch" and genre == "battleroyale" and setup == "livingroom":
+        return "Cloud Alpha"
+    elif answer == "nintendo switch" and genre == "rpg" and setup == "livingroom":
+        return "Razer Kraken" 
+    elif answer == "nintendo switch" and genre == "fps" and setup == "livingroom":
+        return "Arctis 1"
+    elif answer == "nintendo switch" and genre == "sim" and setup == "livingroom":
+        return "Turtle Beach 50P"
+    elif answer == "nintendo switch" and genre == "sim" and setup == "livingroom":
+        return "Logitech G Pro"
+    elif answer == "nintendo switch" and genre == "battleroyale" and setup == "office":
+        return "Cloud Alpha"
+    elif answer == "nintendo switch" and genre == "rpg" and setup == "office":
+        return "Razer Kraken" 
+    elif answer == "nintendo switch" and genre == "fps" and setup == "office":
+        return "Arctis 1"
+    elif answer == "nintendo switch" and genre == "sim" and setup == "office":
+        return "Turtle Beach 50P"
+    elif answer == "nintendo switch" and genre == "sim" and setup == "office":
+        return "Logitech G Pro"
+    elif answer == "ps4" and genre == "battleroyale" and setup == "office":
+        return "Cloud Alpha"
+    elif answer == "ps4" and genre == "rpg" and setup == "office":
+        return "Razer Kraken" 
+    elif answer == "ps4" and genre == "fps" and setup == "office":
+        return "Arctis 1"
+    elif answer == "ps4" and genre == "sim" and setup == "office":
+        return "Turtle Beach 50P"
+    elif answer == "ps4" and genre == "sim" and setup == "office":
+        return "Logitech G Pro"
+    
+    elif answer == "ps4" and genre == "battleroyale" and setup == "desk":
+        return "Cloud Alpha"
+    elif answer == "ps4" and genre == "rpg" and setup == "desk":
+        return "Razer Kraken" 
+    elif answer == "ps4" and genre == "fps" and setup == "desk":
+        return "Arctis 1"
+    elif answer == "ps4" and genre == "sim" and setup == "desk":
+        return "Turtle Beach 50P"
+    elif answer == "ps4" and genre == "sim" and setup == "desk":
+        return "Logitech G Pro"
+    
+    elif answer == "ps4" and genre == "battleroyale" and setup == "livingroom":
+        return "Cloud Alpha"
+    elif answer == "ps4" and genre == "rpg" and setup == "livingroom":
+        return "Razer Kraken" 
+    elif answer == "ps4" and genre == "fps" and setup == "livingroom":
+        return "Arctis 1"
+    elif answer == "ps4" and genre == "sim" and setup == "livingroom":
+        return "Turtle Beach 50P"
+    elif answer == "ps4" and genre == "sim" and setup == "livingroom":
+        return "Logitech G Pro"
+    elif answer == "xbox" and genre == "battleroyale" and setup == "desk":
+        return "Cloud Alpha"
+    elif answer == "xbox" and genre == "rpg" and setup == "desk":
+        return "Razer Kraken" 
+    elif answer == "xbox" and genre == "fps" and setup == "desk":
+        return "Arctis 1"
+    elif answer == "xbox" and genre == "sim" and setup == "desk":
+        return "Turtle Beach 50P"
+    elif answer == "xbox" and genre == "sim" and setup == "desk":
+        return "Logitech G Pro"
+    if answer == "xbox" and genre == "battleroyale" and setup == "livingroom":
+        return "Cloud Alpha"
+    elif answer == "xbox" and genre == "rpg" and setup == "livingroom":
+        return "Razer Kraken" 
+    elif answer == "xbox" and genre == "fps" and setup == "livingroom":
+        return "Arctis 1"
+    elif answer == "xbox" and genre == "sim" and setup == "livingroom":
+        return "Turtle Beach 50P"
+    elif answer == "xbox" and genre == "sim" and setup == "livingroom":
+        return "Logitech G Pro"
+    if answer == "xbox" and genre == "battleroyale" and setup == "office":
+        return "Cloud Alpha"
+    elif answer == "xbox" and genre == "rpg" and setup == "office":
+        return "Razer Kraken" 
+    elif answer == "xbox" and genre == "fps" and setup == "office":
+        return "Arctis 1"
+    elif answer == "xbox" and genre == "sim" and setup == "office":
+        return "Turtle Beach 50P"
+    elif answer == "xbox" and genre == "sim" and setup == "office":
+        return "Logitech G Pro"
+    # elif answer == "pc" and genre == "rpg":
+    #     return "Razer Kraken" 
+    # elif answer == "pc" and genre == "fps":
+    #     return "Arctis 1"
+    # elif answer == "pc" and genre == "sim":
+    #     return "Turtle Beach 550"
+    # elif answer == "pc" and genre == "sim":
+    #     return "Logitech G Pro"
+    # elif answer == "ps4" and genre == "battleroyale":
+    #     return "Cloud Alpha"
+    # elif answer == "ps4" and genre == "rpg":
+    #     return "Arctis 1" 
+    # elif answer == "ps4" and genre == "fps":
+    #     return "Turtle Beach 550"
+    # elif answer == "ps4" and genre == "sim":
+    #     return "Logitech G Pro"
+    # elif answer == "ps4" and genre == "strategy":
+    #     return "Razer Kraken"
+    # elif answer == "xbox" and genre == "battle royale":
+    #     return "Turtle Beach 550"
+    # elif answer == "xbox" and genre == "rpg":
+    #     return "Logitech G Pro"
+    # elif answer == "xbox" and genre == "fps":
+    #     return "Cloud Alpha"
+    # elif answer == "xbox" and genre == "sim":
+    #     return "Razer Kraken"
+    # elif answer == "xbox" and genre == "strategy":
+    #     return "Arctis 1"
+    # elif answer == "phone" and genre == "battleroyale":
+    #     return "Arctis 1"
+    # elif answer == "phone" and genre == "rpg":
+    #     return "Turtle Beach 550"
+    # elif answer == "phone" and genre == "fps":
+    #     return "Razer Kraken"
+    # elif answer == "phone" and genre == "sim":
+    #     return "Cloud Alpha"
+    # elif answer == "phone" and genre == "strategy":
+    #     return "Logitech G Pro"
+    # elif answer == "nintendo switch" and genre == "battleroyale":
+    #     return "Razer Kraken"
+    # elif answer == "nintendo switch" and genre == "rpg":
+    #     return "Cloud Alpha"
+    # elif answer == "nintendo switch" and genre == "fps":
+    #     return "Logitech G Pro"
+    # elif answer == "nintendo switch" and genre == "sim":
+    #     return "Turtle Beach 550"
+    # elif answer == "nintendo switch" and genre == "strategy":
+    #     return "Arctis 1"
+    # # elif answer == "ps4":
+    # #     return "Logitech G Pro"
+    # # elif answer == "xbox":
+    #     return "Razer Kraken"
+    # elif answer == "phone":
+    #     return "Turtle Beach 550"
+    # elif answer == "nintendo switch":
+    #     return "Cloud Alpha"
+    else:
+        return False

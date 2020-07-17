612473b804451b686cb042c373b85c1834241c69
Saad Bensalim <sabensalim@brooklynfriends.org>
2020-07-17 13:57:43 -0400

    changed color

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/quiz')
+@app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
     return render_template('quiz.html', time = datetime.now())
 
